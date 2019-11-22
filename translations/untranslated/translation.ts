bd547778f209a29cba2cdcc06b5241e9665c39c9
Luca Realdi <lr@opencontent.it>
2019-11-22 10:58:55 +0100

    Fix terms link - ticket 2295 (#9)

--- translations/untranslated/translation.ts
+++ translations/untranslated/translation.ts
@@ -293,7 +293,7 @@
 <context>
     <name>sensor/add</name>
     <message>
-        <source>I testi e le immagini inserite dovranno rispettare le policy stabilite per la privacy e i termini di utilizzo</source>
+        <source>I testi e le immagini inserite dovranno rispettare le policy stabilite per %open_privacy_url%la privacy%close_privacy_url% e %open_terms_url%i termini di utilizzo%close_terms_url%</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
