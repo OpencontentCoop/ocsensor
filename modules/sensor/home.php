46003936ffcafb17e037a7048e650682b8913ff4
Luca Realdi <lr@opencontent.it>
2020-02-27 18:02:43 +0100

    Aggiunge expiry per sensor cache

--- modules/sensor/home.php
+++ modules/sensor/home.php
@@ -2,28 +2,27 @@
 
 $module = $Params['Module'];
 
-$tpl = eZTemplate::factory();        
-$tpl->setVariable( 'sensor_home', true );
+$tpl = eZTemplate::factory();
+$tpl->setVariable('sensor_home', true);
 
 $currentUser = eZUser::currentUser();
 
 $ini = eZINI::instance();
-$viewCacheEnabled = ( $ini->variable( 'ContentSettings', 'ViewCaching' ) == 'enabled' );
+$viewCacheEnabled = ($ini->variable('ContentSettings', 'ViewCaching') == 'enabled');
 
-if ( $viewCacheEnabled )
-{
-
-    $cacheFilePath = SensorModuleFunctions::sensorGlobalCacheFilePath( $currentUser->isAnonymous() ? 'home-anon' : 'home' );
-    $cacheFile = eZClusterFileHandler::instance( $cacheFilePath );
-    $Result = $cacheFile->processCache( array( 'SensorModuleFunctions', 'sensorCacheRetrieve' ),
-                                        array( 'SensorModuleFunctions', 'sensorHomeGenerate' ),
-                                        null,
-                                        null,
-                                        compact( 'Params' ) );
-}
-else
-{    
-    $data = SensorModuleFunctions::sensorHomeGenerate( false, compact( 'Params' ) );
-    $Result = $data['content']; 
+if ($viewCacheEnabled) {
+    $expiry = eZExpiryHandler::getTimestamp('template-block-cache', -1);
+    $cacheFilePath = SensorModuleFunctions::sensorGlobalCacheFilePath($currentUser->isAnonymous() ? 'home-anon' : 'home');
+    $cacheFile = eZClusterFileHandler::instance($cacheFilePath);
+    $Result = $cacheFile->processCache(
+        array('SensorModuleFunctions', 'sensorCacheRetrieve'),
+        array('SensorModuleFunctions', 'sensorHomeGenerate'),
+        null,
+        $expiry,
+        compact('Params')
+    );
+} else {
+    $data = SensorModuleFunctions::sensorHomeGenerate(false, compact('Params'));
+    $Result = $data['content'];
 }
 return $Result;
--- modules/sensor/info.php
+++ modules/sensor/info.php
@@ -5,21 +5,21 @@ $identifier = $Params['Page'];
 
 
 $ini = eZINI::instance();
-$viewCacheEnabled = ( $ini->variable( 'ContentSettings', 'ViewCaching' ) == 'enabled' );
+$viewCacheEnabled = ($ini->variable('ContentSettings', 'ViewCaching') == 'enabled');
 
-if ( $viewCacheEnabled )
-{
-    $cacheFilePath = SensorModuleFunctions::sensorGlobalCacheFilePath( 'info-' . $identifier );
-    $cacheFile = eZClusterFileHandler::instance( $cacheFilePath );
-    $Result = $cacheFile->processCache( array( 'SensorModuleFunctions', 'sensorCacheRetrieve' ),
-                                        array( 'SensorModuleFunctions', 'sensorInfoGenerate' ),
-                                        null,
-                                        null,
-                                        compact( 'Params' ) );
-}
-else
-{    
-    $data = SensorModuleFunctions::sensorInfoGenerate( false, compact( 'Params' ) );
-    $Result = $data['content']; 
+if ($viewCacheEnabled) {
+    $expiry = eZExpiryHandler::getTimestamp('template-block-cache', -1);
+    $cacheFilePath = SensorModuleFunctions::sensorGlobalCacheFilePath('info-' . $identifier);
+    $cacheFile = eZClusterFileHandler::instance($cacheFilePath);
+    $Result = $cacheFile->processCache(
+        array('SensorModuleFunctions', 'sensorCacheRetrieve'),
+        array('SensorModuleFunctions', 'sensorInfoGenerate'),
+        null,
+        $expiry,
+        compact('Params')
+    );
+} else {
+    $data = SensorModuleFunctions::sensorInfoGenerate(false, compact('Params'));
+    $Result = $data['content'];
 }
 return $Result;
