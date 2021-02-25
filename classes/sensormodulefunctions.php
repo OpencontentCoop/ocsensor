c936b8fb57377f9c42b06fc604c35956f8f0ce4b
Luca Realdi <lr@opencontent.it>
2021-02-25 15:05:57 +0100

    Modifica il percorso di default per la cache di sensor (per evitare errori usando redis)

--- classes/sensormodulefunctions.php
+++ classes/sensormodulefunctions.php
@@ -3,14 +3,14 @@
 class SensorModuleFunctions
 {
     const GLOBAL_PREFIX = 'global-';
-    
+
     public static function onClearObjectCache( $nodeList )
-    {  
+    {
         try
         {
             $rootNode = SensorHelper::rootNode();
             if ( $rootNode instanceof eZContentObjectTreeNode
-                 && in_array( $rootNode->attribute( 'node_id' ), $nodeList )
+                && in_array( $rootNode->attribute( 'node_id' ), $nodeList )
             )
             {
                 self::clearSensorCache( self::GLOBAL_PREFIX );
@@ -39,12 +39,12 @@ class SensorModuleFunctions
         }
         return $nodeList;
     }
-    
+
     protected static function clearSensorCache( $prefix )
     {
         $ini = eZINI::instance();
         if ( $ini->hasVariable( 'SiteAccessSettings', 'RelatedSiteAccessList' ) &&
-             $relatedSiteAccessList = $ini->variable( 'SiteAccessSettings', 'RelatedSiteAccessList' ) )
+            $relatedSiteAccessList = $ini->variable( 'SiteAccessSettings', 'RelatedSiteAccessList' ) )
         {
             if ( !is_array( $relatedSiteAccessList ) )
             {
@@ -56,29 +56,29 @@ class SensorModuleFunctions
         else
         {
             $siteAccesses = $ini->variable( 'SiteAccessSettings', 'AvailableSiteAccessList' );
-        }            
+        }
         if ( !empty( $siteAccesses ) )
-        {                
-            $cacheBaseDir = eZDir::path( array( eZSys::cacheDirectory(), 'sensor' ) );                
+        {
+            $cacheBaseDir = eZDir::path( array( eZSys::cacheDirectory(), 'ocopendata', 'sensor' ) );
             $fileHandler = eZClusterFileHandler::instance();
             $fileHandler->fileDeleteByDirList( $siteAccesses, $cacheBaseDir, $prefix );
         }
     }
-    
+
     public static function sensorHomeGenerate( $file, $args )
     {
         $currentUser = eZUser::currentUser();
-        
-        $tpl = eZTemplate::factory();        
+
+        $tpl = eZTemplate::factory();
         $tpl->setVariable( 'current_user', $currentUser );
         $tpl->setVariable( 'persistent_variable', array() );
         $tpl->setVariable( 'sensor_home', true );
-        
+
         $Result = array();
         $Result['persistent_variable'] = $tpl->variable( 'persistent_variable' );
         $Result['content'] = $tpl->fetch( 'design:sensor/home.tpl' );
         $Result['node_id'] = 0;
-        
+
         $contentInfoArray = array( 'url_alias' => 'sensor/home' );
         $contentInfoArray['persistent_variable'] = array( 'sensor_home' => true );
         if ( $tpl->variable( 'persistent_variable' ) !== false )
@@ -89,10 +89,10 @@ class SensorModuleFunctions
         $Result['content_info'] = $contentInfoArray;
         $Result['path'] = array();
         $returnValue = array( 'content' => $Result,
-                         'scope'   => 'sensor' );
+            'scope'   => 'sensor' );
         return $returnValue;
     }
-    
+
     public static function sensorInfoGenerate( $file, $args )
     {
         extract( $args );
@@ -151,10 +151,10 @@ class SensorModuleFunctions
             );
         }
         return $returnValue;
-    }    
-    
+    }
+
     public static function sensorPostPopupGenerate( $file, $args )
-    {        
+    {
         $object = eZContentObject::fetch( $args );
         if ( $object instanceof eZContentObject && $object->attribute( 'can_read' ) )
         {
@@ -168,29 +168,29 @@ class SensorModuleFunctions
             $Result = '<em>Private</em>';
         }
         $returnValue = array( 'content' => $Result,
-                              'scope' => 'sensor' );
+            'scope' => 'sensor' );
         return $returnValue;
     }
-    
+
     public static function sensorPostGenerate( $file, $args )
     {
         extract( $args );
-        
+
         try
         {
             $object = eZContentObject::fetch( $postId );
             if ( !$object instanceof eZContentObject )
-            {                
+            {
                 return  array( 'content' => $module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' ),
-                               'store'   => false );
+                    'store'   => false );
             }
 
             if ( !$object->attribute( 'can_read' ) )
-            {                
+            {
                 return  array( 'content' => $module->handleError( eZError::KERNEL_ACCESS_DENIED, 'kernel' ),
-                               'store'   => false );
+                    'store'   => false );
             }
-    
+
             /** @var SensorHelper $helper */
             $helper = SensorHelper::instanceFromContentObjectId( $postId );
             $helper->onRead();
@@ -210,12 +210,12 @@ class SensorModuleFunctions
         {
             $tpl->setVariable( 'error', $e->getMessage() );
         }
-    
+
         $Result = array();
         $Result['persistent_variable'] = $tpl->variable( 'persistent_variable' );
         $Result['content'] = $tpl->fetch( 'design:sensor/post/full.tpl' );
         $Result['node_id'] = 0;
-    
+
         $contentInfoArray = array( 'url_alias' => 'sensor/post/' . $postId );
         $contentInfoArray['persistent_variable'] = false;
         if ( $tpl->variable( 'persistent_variable' ) !== false )
@@ -224,25 +224,25 @@ class SensorModuleFunctions
         }
         $Result['content_info'] = $contentInfoArray;
         $Result['path'] = array();
-        
+
         return array( 'content' => $Result,
-                      'scope'   => 'sensor' );
+            'scope'   => 'sensor' );
     }
 
     public static function sensorCacheRetrieve( $file, $mtime, $args )
     {
-        $Result = include( $file );        
+        $Result = include( $file );
         return $Result;
     }
-    
+
     public static function sensorPostCacheFilePath( $user, $postId, $viewParameters, $cacheNameExtra = '' )
     {
         $cacheHashArray = array();
-        
+
         if ( $user instanceof eZUser )
-        {            
+        {
             $cacheHashArray[] = implode( '.', $user->roleIDList() );
-            
+
             $limitValueList = implode( '.', $user->limitValueList() );
             if ( !empty( $limitValueList ) )
             {
@@ -257,7 +257,7 @@ class SensorModuleFunctions
                 }
             }
         }
-        
+
         //@todo? al momento non serve mettere in cache i viewparameters perché non vengono usati
         //$vpString = '';
         //ksort( $viewParameters );
@@ -268,12 +268,12 @@ class SensorModuleFunctions
         //    $vpString .= 'vp:' . $key . '=' . $value;
         //}
         //$cacheHashArray[] = $vpString;
-        
+
         $currentSiteAccess = $GLOBALS['eZCurrentAccess']['name'];
         $cacheFile = $postId . '-' . $cacheNameExtra . md5( implode( '-', $cacheHashArray ) ) . '.cache';
         $extraPath = eZDir::filenamePath( $postId );
-        $cachePath = eZDir::path( array( eZSys::cacheDirectory(), 'sensor', $currentSiteAccess, 'post', $extraPath, $cacheFile ) );
-        
+        $cachePath = eZDir::path( array( eZSys::cacheDirectory(), 'ocopendata', 'sensor', $currentSiteAccess, 'post', $extraPath, $cacheFile ) );
+
         return $cachePath;
     }
 
@@ -281,7 +281,7 @@ class SensorModuleFunctions
     {
         $currentSiteAccess = $GLOBALS['eZCurrentAccess']['name'];
         $cacheFile = self::GLOBAL_PREFIX . $fileName . '.php';
-        $cachePath = eZDir::path( array( eZSys::cacheDirectory(), 'sensor', $currentSiteAccess, $cacheFile ) );        
+        $cachePath = eZDir::path( array( eZSys::cacheDirectory(), 'ocopendata', 'sensor', $currentSiteAccess, $cacheFile ) );
         return $cachePath;
     }
 }
