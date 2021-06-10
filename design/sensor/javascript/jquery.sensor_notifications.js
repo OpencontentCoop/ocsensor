525bba88dd4c19303d22f0374f5cb87273092523
Luca Realdi <lr@opencontent.it>
2021-06-10 12:32:35 +0200

    Corregge un bug nella visualizzazione delle impostazioni delle notifiche

--- design/sensor/javascript/jquery.sensor_notifications.js
+++ design/sensor/javascript/jquery.sensor_notifications.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
     if (Socket && window.Notification) {
+
+        var availableEvents = ['on_create'];
+
         // control /notification/settings
         var settingsRow = $('#desktop-notification-settings').removeClass('hide');
+        var settingsTable = $('table.desktopNotifications');
+
         settingsRow.find('.notificationPermissionStatus.' + Notification.permission).removeClass('hide');
+        if (Notification.permission === 'granted') {
+            $.each(availableEvents, function () {
+                settingsTable.find('.' + this).removeClass('hide');
+            });
+        }
         settingsRow.find('a.enableNotificationButton').on('click', function (e) {
             Notification.requestPermission().then(function (permission) {
                 settingsRow.find('.notificationPermissionStatus').addClass('hide');
+                settingsTable.find('.desktopNotificationType').addClass('hide');
                 settingsRow.find('.notificationPermissionStatus.' + permission).removeClass('hide');
+                if (permission === 'granted') {
+                    $.each(availableEvents, function () {
+                        settingsTable.find('.' + this).removeClass('hide');
+                    });
+                }
             });
             e.preventDefault();
         });
@@ -14,7 +30,13 @@ $(document).ready(function () {
             navigator.permissions.query({name: 'notifications'}).then(function (notificationPerm) {
                 notificationPerm.onchange = function () {
                     settingsRow.find('.notificationPermissionStatus').addClass('hide');
+                    settingsTable.find('.desktopNotificationType').addClass('hide');
                     settingsRow.find('.notificationPermissionStatus.' + Notification.permission).removeClass('hide');
+                    if (Notification.permission === 'granted') {
+                        $.each(availableEvents, function () {
+                            settingsTable.find('.' + this).removeClass('hide');
+                        });
+                    }
                 };
             });
         }
@@ -25,41 +47,43 @@ $(document).ready(function () {
 
         if (Notification.permission === 'granted') {
             $.getJSON('/api/sensor_gui/users/current', function (user) {
-                Socket.on('on_create', function (data) {
-                    if (!(data.creator === user.id) &&
-                        (
-                            $.inArray(user.id, data.users) > -1
-                            || data.groups.filter(function (n) {
-                                return user.groups.indexOf(n) !== -1;
-                            }).length > 0
-                        )
-                    ) {
-                        var title = $('title').text();
-                        var icon = $('a.navbar-brand img').attr('src');
-                        var options = {
-                            body: '[' + data.id + '] Nuova segnalazione',
-                            icon: icon,
-                            badge: icon,
-                            data: data,
-                            silent: false,
-                            tag: 'post-' + data.id,
-                            requireInteraction: false
-                        };
+                $.each(availableEvents, function (){
+                    Socket.on(this, function (data) {
+                        if (!(data.creator === user.id) &&
+                            (
+                                $.inArray(user.id, data.users) > -1
+                                || data.groups.filter(function (n) {
+                                    return user.groups.indexOf(n) !== -1;
+                                }).length > 0
+                            )
+                        ) {
+                            var title = $('title').text();
+                            var icon = $('a.navbar-brand img').attr('src');
+                            var options = {
+                                body: '[' + data.id + '] Nuova segnalazione',
+                                icon: icon,
+                                badge: icon,
+                                data: data,
+                                silent: false,
+                                tag: 'post-' + data.id,
+                                requireInteraction: false
+                            };
 
-                        if ('serviceWorker' in navigator) {
-                            navigator.serviceWorker.getRegistration(sw).then((reg) => {
-                                options.requireInteraction = true;
-                                reg.showNotification(title, options);
-                            });
-                        } else {
-                            var notification = new Notification(title, options);
-                            notification.onclick = function (event) {
-                                var pageURL = '/sensor/posts/' + this.data.id;
-                                event.preventDefault();
-                                location.href = pageURL;
+                            if ('serviceWorker' in navigator) {
+                                navigator.serviceWorker.getRegistration(sw).then((reg) => {
+                                    options.requireInteraction = true;
+                                    reg.showNotification(title, options);
+                                });
+                            } else {
+                                var notification = new Notification(title, options);
+                                notification.onclick = function (event) {
+                                    var pageURL = '/sensor/posts/' + this.data.id;
+                                    event.preventDefault();
+                                    location.href = pageURL;
+                                }
                             }
                         }
-                    }
+                    })
                 })
             });
         }
--- design/sensor/templates/notification/handler/ezcollaboration/settings/edit.tpl
+++ design/sensor/templates/notification/handler/ezcollaboration/settings/edit.tpl
@@ -3,17 +3,17 @@
 {if fetch('user', 'has_access_to', hash('module','sensor','function','manage'))}
     {set $access_groups = array('standard','operator')}
 {/if}
-<div class="panel panel-default">
 
-    <input type="hidden" name="CollaborationHandlerSelection" value="1"/>
-    {foreach $handlers as $current_handler}
-        {if $current_handler.info.type-identifier|eq(sensor_collaboration_identifier())}
 
-            {if is_array($current_handler.info.notification-types)}
+<input type="hidden" name="CollaborationHandlerSelection" value="1"/>
+{foreach $handlers as $current_handler}
+    {if $current_handler.info.type-identifier|eq(sensor_collaboration_identifier())}
 
-                <table class="table table-striped">
+        {if is_array($current_handler.info.notification-types)}
 
-                    {if and(fetch('user', 'has_access_to', hash('module','sensor','function','manage')), sensor_settings('SocketIsEnabled'))}
+            {if and(fetch('user', 'has_access_to', hash('module','sensor','function','manage')), sensor_settings('SocketIsEnabled'))}
+            <div class="panel panel-default">
+                <table class="table">
                     <tr id="desktop-notification-settings" class="hide">
                         <td width="1"><i class="fa fa-desktop"></i></td>
                         <td>
@@ -32,37 +32,43 @@
                             </span>
                         </td>
                     </tr>
-                    {/if}
-
-                    {foreach $current_handler.info.notification-types as $type}
-
-                        {if $access_groups|contains($type.group)}
-                            <tr>
-                                <td width="1">
-                                    <input type="checkbox"
-                                           name="CollaborationHandlerSelection_{$handler.id_string}[]"
-                                           value="{$current_handler.info.type-identifier}_{$type.identifier}" {if $selection|contains(concat($current_handler.info.type-identifier,'_',$type.identifier))} checked="checked"{/if} />
-                                </td>
-                                <td>
-                                    {$type.name|wash}
-                                    {if is_set($type.description)}
-                                        <br/>
-                                        <small>{$type.description|wash()}</small>{/if}
-                                </td>
-                            </tr>
-                        {/if}
-                    {/foreach}
                 </table>
+            </div>
             {/if}
 
+            <div class="panel panel-default">
+                <table class="table table-striped desktopNotifications">
+                {foreach $current_handler.info.notification-types as $type}
+                    {if $access_groups|contains($type.group)}
+                        <tr>
+                            <td width="1">
+                                <input type="checkbox"
+                                       name="CollaborationHandlerSelection_{$handler.id_string}[]"
+                                       value="{$current_handler.info.type-identifier}_{$type.identifier}" {if $selection|contains(concat($current_handler.info.type-identifier,'_',$type.identifier))} checked="checked"{/if} />
+                            </td>
+                            <td>
+                                {$type.name|wash}
+                                {if is_set($type.description)}
+                                    <br/>
+                                    <small>{$type.description|wash()}</small>{/if}
+                            </td>
+                            <td style="vertical-align: middle">
+                                <div class="{$type.identifier} hide desktopNotificationType">
+                                    <i class="fa fa-desktop"></i>
+                                </div>
+                            </td>
+                        </tr>
+                    {/if}
+                {/foreach}
+                </table>
+                <div class="panel-footer">
+                    <input class="button btn btn-xs btn-success" type="submit" name="Store"
+                           value="{'Salva le impostazioni'|i18n('sensor/settings')}"/>
+                </div>
+            </div>
+        {/if}
         {/if}
     {/foreach}
 
-    <div class="panel-footer">
-        <input class="button btn btn-xs btn-success" type="submit" name="Store"
-               value="{'Salva le impostazioni'|i18n('sensor/settings')}"/>
-    </div>
 
-
-</div>
 
