bead24058985aed558e175cb6a936999554610e8
Luca Realdi <lr@opencontent.it>
2021-10-11 09:03:28 +0200

    Nasconde il codice fiscale dall'export csv

--- classes/exporters/csv.php
+++ classes/exporters/csv.php
@@ -54,7 +54,7 @@ class SensorPostCsvExporter extends SearchQueryCSVExporter
             'resolution_diff' => ezpI18n::tr('sensor/export', 'Tempo di risoluzione'),
             'title' => ezpI18n::tr('sensor/export', 'Titolo'),
             'author' => ezpI18n::tr('sensor/export', 'Autore'),
-            'fiscal_code' => ezpI18n::tr('sensor/export', 'Codice Fiscale'),
+            // 'fiscal_code' => ezpI18n::tr('sensor/export', 'Codice Fiscale'),
             'category' => ezpI18n::tr('sensor/export', 'Categoria'),
             'category_child' => ezpI18n::tr('sensor/export', 'Categoria (descrittore)'),
             'current_owner' => ezpI18n::tr('sensor/export', 'Incaricato'),
@@ -122,7 +122,7 @@ class SensorPostCsvExporter extends SearchQueryCSVExporter
                 'resolution_diff' => $post->resolutionInfo->resolutionDateTime instanceof DateTime ? $post->resolutionInfo->text : '',
                 'title' => $post->subject,
                 'author' => $post->author->name,
-                'fiscal_code' => $post->author->fiscalCode,
+                // 'fiscal_code' => $post->author->fiscalCode,
                 'category' => count($post->categories) > 0 ? $this->mainCategories[$post->categories[0]->id] : '',
                 'category_child' => count($post->categories) > 0 && isset($this->childCategories[$post->categories[0]->id]) ? $this->childCategories[$post->categories[0]->id] : '',
                 'current_owner' => $post->owners->count() > 0 ? $post->owners->first()->name : '',
@@ -132,6 +132,7 @@ class SensorPostCsvExporter extends SearchQueryCSVExporter
             if ($this->searchPolicies !== null && ($post->privacy->identifier != 'public' || $post->moderation->identifier == 'waiting')){
                 $item['title'] = '***';
                 $item['author'] = '***';
+                // $item['fiscal_code'] = '***';
             }
 
             return $item;
