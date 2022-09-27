80bc49ba4c87ba6b1b52fcebac1f1bc9acb7784f
Luca Realdi <luca.realdi@opencontent.it>
2022-09-28 00:21:46 +0200

    Multi db siteaccess with domain sharing workaround

--- classes/api/pre_routing_filter.php
+++ classes/api/pre_routing_filter.php
@@ -3,17 +3,23 @@
 class SensorPreRoutingFilter implements ezpRestPreRoutingFilterInterface
 {
     public function __construct( ezcMvcRequest $request )
-    {        
+    {
         if ( strpos( $request->requestId, 'api/sensor' ) !== false )
-        {                                    
+        {
             eZDebug::setHandleType( eZDebug::HANDLE_FROM_PHP );
-            $currentSiteaccess = eZSiteAccess::current();
-            $sensorSiteAccessName = ObjectHandlerServiceControlSensor::getSensorSiteAccessName();
-            if ( $currentSiteaccess['name'] !== $sensorSiteAccessName )
+            $currentSiteAccess = eZSiteAccess::current();
+            $currentSiteAccessName = $currentSiteAccess['name'];
+            $sensorRedirectFile = eZSys::rootDir() . "/settings/siteaccess/{$currentSiteAccessName}/sensor_api_redirect";
+            if (file_exists($sensorRedirectFile)){
+                $sensorSiteAccessName = trim(file_get_contents($sensorRedirectFile));
+            }else {
+                $sensorSiteAccessName = ObjectHandlerServiceControlSensor::getSensorSiteAccessName();
+            }
+            if ( $currentSiteAccessName !== $sensorSiteAccessName )
             {
-                eZINI::resetAllInstances();            
-                eZExtension::activateExtensions( 'default' );            
-                eZSiteAccess::change( array( 'name' => $sensorSiteAccessName ) );            
+                eZINI::resetAllInstances();
+                eZExtension::activateExtensions( 'default' );
+                eZSiteAccess::change( array( 'name' => $sensorSiteAccessName ) );
                 eZExtension::activateExtensions( 'access' );
             }
         }
