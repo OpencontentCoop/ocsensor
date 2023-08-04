1614b99d7c4beb75d7037ee04e3cf529658e093d
Luca Realdi <luca.realdi@opencontent.it>
2023-08-04 14:04:10 +0200

    Fix Nominatim api paths

--- design/sensor/javascript/Control.Geocoder.js
+++ design/sensor/javascript/Control.Geocoder.js
@@ -378,7 +378,7 @@
         },
 
         geocode: function (query, cb, context) {
-            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'search/', L.extend({
+            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'search', L.extend({
                     q: query,
                     limit: 10,
                     format: 'json',
@@ -405,7 +405,7 @@
         },
 
         reverse: function (location, scale, cb, context) {
-            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'reverse/', L.extend({
+            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'reverse', L.extend({
                 lat: location.lat,
                 lon: location.lng,
                 zoom: Math.round(Math.log(scale / 256) / Math.log(2)),
@@ -496,7 +496,7 @@
         },
 
         geocode: function (query, cb, context) {
-            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'search/', L.extend(query, {
+            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'search', L.extend(query, {
                     limit: 5,
                     format: 'json',
                     addressdetails: 1
@@ -522,7 +522,7 @@
         },
 
         reverse: function (location, scale, cb, context) {
-            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'reverse/', L.extend({
+            L.Control.Geocoder.jsonp(this.options.serviceUrl + 'reverse', L.extend({
                 lat: location.lat,
                 lon: location.lng,
                 zoom: Math.round(Math.log(scale / 256) / Math.log(2)),
